9429838b54f3d420c5a938aadfcbceb829bedcdd
Thomas Faddegon <tomfaddegon@gmail.com>
2023-07-02 23:26:34 -0400

    Time: 55 ms (79.26%) | Memory: 42 MB (46.72%) - LeetSync

---
+++ remove-element/remove-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let left = 0;
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== val) {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+
+    return left;
+};
