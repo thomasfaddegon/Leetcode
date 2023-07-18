74880acc5dd4ac7088b459ce776c916d574d2056
Thomas Faddegon <tomfaddegon@gmail.com>
2023-07-18 19:16:59 -0400

    Time: 97 ms (87.43%) | Memory: 49.7 MB (57.93%) - LeetSync

---
+++ 155-min-stack/min-stack.js
@@ -0,0 +1,57 @@
+// the key is to use two arrays in minStack, one for the complete stack and one for minStack
+// you could save the minimum in variable, but what if you pop it off? need to know what the next min is
+// when pushing, compare the pushed val to last value of the minStack
+// if it's less than or equal to, push the val BUT if it's not, just push another instance of the last value 
+// This way the "top" value in the stack or the last in the array is always the minimum
+
+var MinStack = function() {
+    this.stack = []
+    this.minStack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val)
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]){
+        this.minStack.push(val)
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+     const popped = this.stack.pop();
+    if (popped === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+     if (this.minStack.length === 0) {
+        return undefined; // or any other designated value
+    }
+    return this.minStack[this.minStack.length - 1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
